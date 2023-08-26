22d0a504fb9a8617f9fe0c22e049623f48739154
Alex Larikov <alexander@larikov.com>
2023-08-26 13:38:40 +0800

    Ability to work with email templates

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import logging
 
+import jinja2
 from fastapi import FastAPI
 from sentry_sdk.integrations.redis import RedisIntegration
 
@@ -28,3 +29,8 @@ logging.basicConfig(
 app = FastAPI(title=settings.title, version=settings.version, debug=settings.debug)
 
 app.include_router(router, prefix="/v1")
+
+templates = jinja2.Environment(
+    loader=jinja2.PackageLoader("app", "templates"),
+    autoescape=jinja2.select_autoescape(["html", "txt"]),
+)
