a119eda071a196afda104de62b473624d6814411
Emin Mammadov <emammadov@acerta.ca>
2023-09-05 17:33:12 -0400

    Fixed tests to test code splitter

--- app/v1/endpoints/user.py
+++ app/v1/endpoints/user.py
@@ -90,7 +90,9 @@ async def register(
         username=user.username,
         email=user.email,
         access_token=await generate_jwt_access_token(user),
-        refresh_token=await generate_jwt_refresh_token(user=user, jti=user_session.uuid),
+        refresh_token=await generate_jwt_refresh_token(
+            user=user, jti=user_session.uuid
+        ),
     )
 
 
