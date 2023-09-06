c98576f1837d9a40914a12fb85976a12bb6de50b
Alex Larikov <alexander@larikov.com>
2023-09-06 20:53:42 +0800

    Login endpoint and tests

--- app/v1/endpoints/user.py
+++ app/v1/endpoints/user.py
@@ -1,19 +1,20 @@
 import logging
 from http import HTTPStatus
-from typing import Annotated
 
-from fastapi import APIRouter, Depends, Header, HTTPException, Request
+from fastapi import APIRouter, Depends, HTTPException, Request
 from redis.asyncio import Redis
 from sqlalchemy.orm import Session
 from tenacity import RetryError
 
 from app import deps
 from app.core.utils.security import (
+    check_password,
     generate_and_email_confirmation_code,
     generate_jwt_access_token,
     generate_jwt_refresh_token,
 )
 from app.crud import crud_user, crud_user_session
+from app.custom_types.request_types import UserAgent
 from app.external.exceptions import MonolithUserCreateException
 from app.schemas import user_schema
 from app.schemas.response_schema import HTTPResponse
@@ -47,7 +48,7 @@ async def register(
     request: Request,
     db: Session = Depends(deps.get_db),
     redis: Redis = Depends(deps.get_redis),
-    user_agent: Annotated[str | None, Header()] = None,
+    user_agent: UserAgent = None,
 ):
     """Регистрирует пользователя в базе Auth service и отправляет его на монолит.
 
@@ -94,6 +95,55 @@ async def register(
     )
 
 
-@router.post("/login")
-def login():
-    ...
+@router.post(
+    "/login",
+    response_model=user_schema.UserWithJWT,
+    status_code=HTTPStatus.OK,
+    responses={
+        HTTPStatus.OK: {
+            "model": user_schema.UserWithJWT,
+            "description": "Пользователь успешно авторизован.",
+        },
+        HTTPStatus.UNAUTHORIZED: {
+            "model": HTTPResponse,
+            "description": "Пользователь с такими данными не существует.",
+        },
+    },
+)
+async def login(
+    payload: user_schema.UserLoginData,
+    request: Request,
+    db: Session = Depends(deps.get_db),
+    user_agent: UserAgent = None,
+):
+    """Позволяет пользователю залогиниться и получить access и refresh токены.
+
+    - `username` - Может содержать имя пользователя, либо email.
+    - `password` - Пароль
+    """
+    user = await crud_user.get_by_username(db, payload.username)
+    if not user:
+        user = await crud_user.get_by_email(db, payload.username)
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Пользователь с таким данными не зарегистрирован.",
+        )
+
+    if not check_password(payload.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Пользователь с таким данными не зарегистрирован.",
+        )
+
+    user_session = await crud_user_session.create_user_session(
+        db=db, user=user, request=request, user_agent=user_agent
+    )
+    return user_schema.UserWithJWT(
+        uuid=user.uuid,
+        username=user.username,
+        email=user.email,
+        access_token=await generate_jwt_access_token(user),
+        refresh_token=await generate_jwt_refresh_token(user=user, jti=user_session.uuid),
+    )
